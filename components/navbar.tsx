9ee92da06255afc615542c08a629dfa86b3cebcc
Mauro Goyeneche <maurogoyeneche@gmail.com>
2024-08-25 18:01:03 -0300

    nav static

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,8 +8,8 @@ import { ThemeSwitch } from "@/components/theme-switch";
 
 export const Navbar = () => {
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="flex  justify-center w-full mt-9">
+    <NextUINavbar maxWidth="xl" position="static">
+      <NavbarContent className="flex  justify-center w-full mt-4 p-4">
         <NavbarItem className="flex gap-2 justify-center w-full">
           <ThemeSwitch />
         </NavbarItem>
