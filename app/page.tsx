30a315dd6c4c32bd168ddc4d801dddb3ea834098
Mauro Goyeneche <maurogoyeneche@gmail.com>
2024-08-25 17:53:38 -0300

    layout adjust

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center w-5/6 ">
-        <h1 className={title()}>Te invitamos a&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>Colaborar&nbsp;</h1>
+        <h1 className={title()}>Te invitamos a &nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>Colaborar</h1>
         <br />
         <h1 className={title({ size: "sm" })}>
           aprendiendo trucos de peluquería para mejorar día a dia.
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ import { ThemeSwitch } from "@/components/theme-switch";
 export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="flex  justify-center w-full">
+      <NavbarContent className="flex  justify-center w-full mt-9">
         <NavbarItem className="flex gap-2 justify-center w-full">
           <ThemeSwitch />
         </NavbarItem>
