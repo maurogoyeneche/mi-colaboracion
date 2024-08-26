43b25f76f389689cf228d7152568b9ca818ab58a
Mauro Goyeneche <maurogoyeneche@gmail.com>
2024-08-25 22:27:25 -0300

    style in white mode

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <h1 className={title({ size: "sm" })}>
           aprendiendo trucos de peluquería para mejorar en día a día.
         </h1>
-        <h2 className="mt-8 text-lg text-zinc-300 italic">
+        <h2 className="mt-8 text-lg text-zinc-900 dark:text-zinc-300 italic">
           Aprendamos... Seamos
           <span className="text-white font-semibold">1%</span> mejor que ayer,
           ayudando a otros.
